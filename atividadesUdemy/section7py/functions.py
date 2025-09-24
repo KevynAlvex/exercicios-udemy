422eab6058c1d5a5df423f74d5cf9c0630eff86e
Kevyn Alves <gkevyn.alves@gmail.com>
2025-09-23 23:05:39 -0300

    initial commit

---
+++ atividadesUdemy/section7py/functions.py
@@ -0,0 +1,13 @@
+def vertical(String):
+    for i in String:
+        print(i)
+
+def pesos(peso):
+    if peso <= 20:
+        if peso <= 10:
+            print('O valor será 50$')
+        else:
+            print('O valor será 80$')
+    else:
+        print('O transporte não será aceito')
+
---
+++ ex001/main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+dataFrame = pd.read_csv('D:\python-data-science\ex001\despesas.csv')
+
+print(dataFrame.head())
